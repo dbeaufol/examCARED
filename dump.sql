51fb051f8407efa3dc3e860c971cb827b3ac8793
Beaufol <denisbeaufol07@yahoo.fr>
2017-02-10 14:45:11 +0000

    evaluation carred commit

--- dump.sql
+++ dump.sql
@@ -1,4 +1,1 @@
-Usage: mysqldump [OPTIONS] database [tables]
-OR     mysqldump [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
-OR     mysqldump [OPTIONS] --all-databases [OPTIONS]
-For more options, use mysqldump --help
+Ctrl-C -- exit!
